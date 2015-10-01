9788011da07201cb9706fe18d603c5ebc9045113
Ronald Fischer <rff@ronalds-mbp.attlocal.net>
2015-10-01 09:50:32 -0700

    First version of a good Swift/iWatch project

---
+++ BitWatch WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,65 @@
+//
+//  InterfaceController.swift
+//  BitWatch WatchKit Extension
+//
+//  Created by Ronald Fischer on 10/1/15.
+//  Copyright (c) 2015 Razeware LLC. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import BitWatchKit
+
+
+class InterfaceController: WKInterfaceController {
+    let tracker = Tracker()
+    var updating = false
+
+    @IBOutlet weak var priceLabel: WKInterfaceLabel!
+    
+    @IBAction func refreshTapped() {
+        update()
+    }
+    
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+        updatePrice(tracker.cachedPrice())
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+        
+        update()
+
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+    
+    private func updatePrice(price: NSNumber) {
+        priceLabel.setText(Tracker.priceFormatter.stringFromNumber(price))
+    }
+
+    private func update() {
+        // 1
+        if !updating {
+            updating = true
+            // 2
+            let originalPrice = tracker.cachedPrice()
+            // 3
+            tracker.requestPrice { (price, error) -> () in
+                // 4
+                if error == nil {
+                    self.updatePrice(price!)
+                }
+                self.updating = false
+            }
+        }
+    }
+
+}
